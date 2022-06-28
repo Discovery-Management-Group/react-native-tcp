78eed7e90bc58d3d368fb029bbd3c93047c91f52
Brett Smith <brettmsmith@protonmail.com>
2022-06-27 21:27:30 -0700

    Stop useless console logging

--- TcpSocket.js
+++ TcpSocket.js
@@ -67,7 +67,7 @@ TcpSocket.prototype._debug = function () {
   if (__DEV__) {
     var args = [].slice.call(arguments);
     args.unshift('socket-' + this._id);
-    console.log.apply(console, args);
+    //console.log.apply(console, args); //please kill this
   }
 };
 
